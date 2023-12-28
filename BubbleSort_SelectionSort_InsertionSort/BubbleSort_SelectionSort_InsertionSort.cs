c234527302c5e4e9f7200b57712202bb7e64c86f
Vero <Vero@WIN-JV3HDVOJ71D>
2023-12-28 12:55:37 +0400

    Добавьте файлы проекта.

---
+++ BubbleSort_SelectionSort_InsertionSort/BubbleSort_SelectionSort_InsertionSort.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort_SelectionSort_InsertionSort
+{
+    public class BubbleSort_SelectionSort_InsertionSort
+    {
+        //Bubble
+        public static void BubbleSort(int[] arr)
+        {
+            int cnt = arr.Length;
+            bool sorted = false;
+
+            while (!sorted)
+            {
+                sorted = true;
+                for (int i = 0; i < cnt - 1; i++)
+                {
+                    if (arr[i] > arr[i + 1])
+                    {
+                        int t = arr[i];
+                        arr[i] = arr[i + 1];
+                        arr[i + 1] = t;
+                        sorted = false;
+                    }
+                }
+                cnt--;
+            }
+        }
+
+        //Selection
+        public static void SelectionSort(int[] arr1)
+        {
+            int ndx = 0, minPos = 0, cnt = arr1.Length;
+
+            while (ndx < cnt - 1)
+            {
+                minPos = ndx;
+
+                
+                for (int i = ndx + 1; i < cnt; i++)
+                {
+                    if (arr1[i] < arr1[minPos])
+                    {
+                        minPos = i;
+                    }
+                }
+
+                int t = arr1[ndx];
+                arr1[ndx] = arr1[minPos];
+                arr1[minPos] = t;
+                ndx++;
+            }
+        }
+
+        //Insertion
+        public static void InsertionSort(int[] arr3)
+        {
+            int cnt = arr3.Length;
+            int key;
+            for (int i = 0; i < cnt; i++)
+            {
+                key = arr3[i];
+                int j = i - 1;
+                while (j >= 0 && arr3[j] > key)
+                {
+                    arr3[j + 1] = arr3[j];
+                    j = j - 1;
+                }
+
+                arr3[j + 1] = key;
+            }
+        }
+    }
+}
---
+++ BubbleSort_SelectionSort_InsertionSort/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Collections.Specialized.BitVector32;
+
+namespace BubbleSort_SelectionSort_InsertionSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //BubbleSort
+            Console.WriteLine("BubbleSort");
+            Console.WriteLine("Enter the number of elements:");
+
+            if (!int.TryParse(Console.ReadLine(), out int n) || n <= 0)
+            {
+                Console.WriteLine("Invalid input. Please enter a positive integer for the number of elements.");
+            }
+
+            int[] a = new int[n];
+
+            Console.WriteLine("Enter the elements:");
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Element {i + 1}: ");
+
+                if (!int.TryParse(Console.ReadLine(), out a[i]))
+                {
+                    Console.WriteLine("Invalid input. Please enter valid integers for the elements.");
+                }
+            }
+
+            BubbleSort_SelectionSort_InsertionSort.BubbleSort(a);
+
+            Console.WriteLine("Sorted array:");
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.WriteLine($"{a[i]}");
+            }
+            Console.WriteLine("________________________________________");
+            Console.WriteLine();
+
+
+
+            //SelectionSort
+            Console.WriteLine("SelectionSort");
+            Console.WriteLine("Enter the number of elements:");
+
+            if (!int.TryParse(Console.ReadLine(), out int m) || m <= 0)
+            {
+                Console.WriteLine("Invalid input. Please enter a positive integer for the number of elements.");
+            }
+
+            int[] arr1 = new int[m];
+
+            Console.WriteLine("Enter the elements:");
+
+            for (int i = 0; i < m; i++)
+            {
+                Console.Write($"Element {i + 1}: ");
+
+                if (!int.TryParse(Console.ReadLine(), out arr1[i]))
+                {
+                    Console.WriteLine("Invalid input. Please enter valid integers for the elements.");
+                }
+            }
+
+            BubbleSort_SelectionSort_InsertionSort.SelectionSort(arr1);
+
+            Console.WriteLine("Sorted array:");
+
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                Console.WriteLine($"{arr1[i]}");
+            }
+            Console.WriteLine("_____________________");
+
+
+
+
+            //InsertionSort
+            Console.WriteLine("InsertionSort");
+            Console.WriteLine("Enter the number of elements:");
+
+            if (!int.TryParse(Console.ReadLine(), out int k) || k <= 0)
+            {
+                Console.WriteLine("Invalid input. Please enter a positive integer for the number of elements.");
+            }
+
+            int[] arr2 = new int[k];
+
+            Console.WriteLine("Enter the elements:");
+
+            for (int i = 0; i < k; i++)
+            {
+                Console.Write($"Element {i + 1}: ");
+
+                if (!int.TryParse(Console.ReadLine(), out arr2[i]))
+                {
+                    Console.WriteLine("Invalid input. Please enter valid integers for the elements.");
+                }
+            }
+            BubbleSort_SelectionSort_InsertionSort.InsertionSort(arr2);
+
+            Console.WriteLine("Sorted array:");
+
+            for (int i = 0; i < arr2.Length; i++)
+            {
+                Console.WriteLine($"{arr2[i]}");
+            }
+            Console.WriteLine("_____________________");
+        }
+    }
+}
